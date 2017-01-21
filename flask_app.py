10e53c8e6299a76509456f706d590ef94f5b3007
cakoch10 <cakoch10@gmail.com>
2017-01-21 13:54:02 +0000

    added HTTP request function

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,27 @@
 from flask import Flask, request, redirect
 import twilio.twiml
+# import httplib, urllib, base64
+
+#headers = {
+#    # Request headers
+#    'Content-Type': 'application/json',
+#    'Ocp-Apim-Subscription-Key': '{subscription key}',
+#}
+
+#params = urllib.urlencode({
+#})
+
+#try:
+#    conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
+#    conn.request("POST", "/emotion/v1.0/recognize?%s" % params, "{body}", headers)
+#    response = conn.getresponse()
+#    data = response.read()
+#    print(data)
+#    conn.close()
+#except Exception as e:
+#    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+
 app = Flask(__name__)
 
 # Try adding your own number to this list!
@@ -21,7 +43,7 @@ def hello_monkey():
 
     resp = twilio.twiml.Response()
     msg = resp.message(mess)
-    msg.media("https://chart.googleapis.com/chart?cht=tx&chl=" + request.values['Body'])
+    msg.media("http://www.mathtran.org/cgi-bin/mathtran?D=15;tex=$x=1$")
     print(request.values['Body'])
     # 'http://www.mathtran.org/cgi-bin/mathtran?D=15;tex=$x=1$'
     
