17a08c975c7d2c79a12b816f99bb47337e92a522
cakoch10 <cakoch10@gmail.com>
2017-01-21 18:07:52 -0800

    updated image retreival

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,26 @@
 from flask import Flask, request, redirect, render_template_string, url_for
 import twilio.twiml
 from sympy import symbols,preview,Symbol
-from tempfile import NamedTemporaryFile
-from shutil import copyfileobj
+import PIL
 from PIL import Image
 import requests
 from io import BytesIO
+from flask_images import resized_img_src
+import os
+import random
+import string
+
+def formula_as_file( formula, file, negate=False ):
+    tfile = file
+    if negate:
+        tfile = 'tmp.png'
+    r = requests.get( 'http://latex.codecogs.com/png.latex?\dpi{300} \huge %s' % formula )
+    f = open( tfile, 'wb' )
+    f.write( r.content )
+    f.close()
+    if negate:
+	#os.system( 'convert tmp.png -negate -colorspace (255,255,255,255) %s' %file )
+        os.system( 'convert tmp.png -channel RGB -negate -colorspace rgb %s' %file )
 
 # import httplib, urllib, base64
 
@@ -30,6 +45,7 @@ from io import BytesIO
 
 
 app = Flask(__name__)
+#app.config['IMAGES_PATH']='static'
 
 # Try adding your own number to this list!
 callers = {
@@ -47,27 +63,41 @@ def showfile(path):
 def hello_monkey():
     """Respond and greet the caller by name."""
 
-    from_number = request.values.get('From', None)
-    if from_number in callers:
-        mess = callers[from_number] + ", thanks for the message!"
-    else:
-        mess = "Monkey, thanks for the message!"
-
     resp = twilio.twiml.Response()
-    msg = resp.message(mess)
+    msg = resp.message("")
     
-    preview(r"$$\int_0^3 x^3 \,dx$$",viewer='file',filename='static/test.png',euler=False)
-    #tempFileObj = NamedTemporaryFile(suffix='png')
-    #pilImage = open('/home/linuxc/git-repos/pennapps/test.png','rb')
-    #copyfileobj(pilImage,tempFileObj)
-    #pilImage.close()
-    
+    title = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
 
-    #img = Image.open('test.png')
+    #preview(r"" + request.values['Body'] + "",viewer='file',filename='static/test.png',euler=False)
+    #formula_as_file( r"\Gamma_{Levin}(x) = \| \nabla p(x) \|_2^{0.8} + \sum_i |\frac{\partial^2 p(x)}{\partial x_i^2}|^{0.8}", "static/" + title + ".png", True)
+    formula_as_file( r"" + request.values['Body'], "static/" + title + ".png", True)
+    
+    
+    img = Image.open('static/' + title + '.png','r')
+    img_w,img_h = img.size
+    background = Image.new('RGBA',(img_w+25,img_h+25),(255,255,255,255))
+    bg_w,bg_h = background.size
+    offset = ((bg_w - img_w)/2,(bg_h - img_h)/2)
+    background.paste(img,offset)
+   
+    title2 = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+   
+    background.save('static/' + title2 + '.png')
+    basewidth = 80
+    #wpercent = (basewidth/float(img.size[0]))
+    #hsize = float(img.size[1]*float(wpercent))
+    
+    #img = img.resize((80,40),PIL.Image.ANTIALIAS)
+    #img.save('static/test2.png', 'PNG', quality=90)
+    
+    img = Image.open('static/' + title2 + '.png')
+    
     #msg.media(img)
     #response = requests.get('http://www.mathtran.org/cgi-bin/mathtran?D=15;tex=$x=1$')
     #img = Image.open(BytesIO(response.content))
-    msg.media(showfile('test.png'))
+    #msg.media(resized_img_src(showfile('test.png'),width=50,height=50))..
+    msg.media(showfile(title2 + '.png'))
+    #msg.media(resized_img_src('test.png',width=50,height=50))
     print(request.values['Body'])
     # 'http://www.mathtran.org/cgi-bin/mathtran?D=15;tex=$x=1$'
     
