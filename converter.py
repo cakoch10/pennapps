3fef4a1323a7c433b3704407a9d1a936fdf96551
lauralindy <laura.f.lin@gmail.com>
2017-01-22 07:22:26 -0500

    fixing things

---
+++ converter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import sys
+import requests
+import json
+import urllib
+
+def main(Media med):
+
+    #file_path = sys.argv[1]
+    urllib.urlretrieve(med, "ABC.jpg")
+
+    r = requests.post('http://api.mathpix.com/v2/latex',
+        files={'file': open(ABC.jpg, 'rb')},
+        headers={"app_id": "4985f625", "app_key": "4423301b832793e217d04bc44eb041d3"})
+    text = r.text
+    returnjson.dumps(json.loads(text), indent=4, sort_keys=True)
